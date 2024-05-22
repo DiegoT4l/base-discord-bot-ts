c917315a20f6baab6e14d8d451ba44ca8c22b26e
DiegoT4l <128425675+DiegoT4l@users.noreply.github.com>
2024-05-22 16:55:49 -0600

    chore: Update package.json scripts and devDependencies

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 // import discord.js
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events } from 'discord.js';
 
 // create a new Client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: 32767 });  // GatewayIntentBits.All
 
 // listen for the client to be ready
 client.once(Events.ClientReady, (c) => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// listen for the message event
+client.on('messageCreate', message => {
+    console.log(message.content);
 });
 
 // login with the token from .env.local
